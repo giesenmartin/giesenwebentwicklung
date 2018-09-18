4632211c931c787a261fe6965b10cde81172fa35
giesenmartin <info@martin-giesen.de>
2018-09-18 18:10:31 +0200

    Initialer Commit

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['Giesen'] = 'EXT:giesenwebentwicklung/Configuration/Rte/customrte.yaml';
